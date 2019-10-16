642d59c1ad6ac2883cf2a9f07289a7b2cc866206
Choyongkeun <pllpllgp@gmail.com>
2019-10-16 15:14:16 +0900

    restapi project create

---
+++ src/test/java/web/app/demorestapi/DemoRestapiApplicationTests.java
@@ -0,0 +1,16 @@
+package web.app.demorestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoRestapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
